a5855a41caafbb52693bdcb247e9fa546c2c0da2
[zaynahb06] <[zbello@uncc.edu]>
2023-12-09 14:36:07 -0500

    new

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,55 +1,41 @@
-window.onload = function () {
-    let calculationCompleted = false;
+/* reference: https://www.makeuseof.com/build-a-simple-calculator-using-html-css-javascript/ */
 
-    const display = document.querySelector('input[name="textNumber"]');
-    display.value = '0';
-
-
-    function isOperator(char) {
-        return ['+', '-', '*', '/'].includes(char);
-    }
-
-    // number input buttons
-    document.querySelectorAll('.num, .decimal, .operator').forEach(button => {
-        button.addEventListener('click', function () {
-            const btnValue = this.textContent || this.innerText;
-
-            if (calculationCompleted && !this.classList.contains('decimal') && !this.classList.contains('operator')) {
-                display.value = btnValue;
-                calculationCompleted = false; // reset
-            } else if (display.value === '0' && !this.classList.contains('decimal') && !this.classList.contains('operator')) {
-                display.value = btnValue;
-            } else if (this.classList.contains('operator')) {
-                if (isOperator(display.value[display.value.length - 1])) {
-                    // remove last operator
-                    display.value = display.value.substring(0, display.value.length - 1);
-                }
-                display.value += btnValue; // add neww operator
-            } else {
-                display.value += btnValue;
-            }
-        });
-    });
-
-    function isOperator(char) {
-        return ['+', '-', 'x', '÷'].includes(char);
-    }
-
-
-
-    // input/output clear button
-    document.querySelector('.clear').addEventListener('click', function () {
-        display.value = '0';
-    });
-
-    // equals button
-    document.querySelector('.equals').addEventListener('click', function () {
-        try {
-            let expression = display.value.replace('x', '*').replace('÷', '/');
-            display.value = eval(expression);
-            calculationCompleted = true;
-        } catch (e) {
-            display.value = "Error";
-        }
-    });
+function display(value) {
+    const calculation = document.getElementById("calculation");
+    calculation.value += value;
 }
+
+function clearScreen() {
+    const calculation = document.getElementById("calculation");
+    calculation.value = '';
+}
+
+function calculate() {
+    const calculation = document.getElementById('calculation');
+    try {
+        calculation.value = eval(calculation.value);
+    } catch (error) {
+        calculation.value = 'Cannot Calculate!';
+    }
+}
+
+document.getElementById("btn").addEventListener("click", clearScreen);
+document.getElementById("calculate").addEventListener("click", calculate);
+document.getElementById("number1").addEventListener("click", function () {display(1);});
+document.getElementById("number2").addEventListener("click", function () {display(2);});
+document.getElementById("number3").addEventListener("click", function () {display(3);});
+
+document.getElementById("number4").addEventListener("click", function () {display(4);});
+document.getElementById("number5").addEventListener("click", function () {display(5);});
+document.getElementById("number6").addEventListener("click", function () {display(6);});
+
+document.getElementById("number7").addEventListener("click", function () {display(7);});
+document.getElementById("number8").addEventListener("click", function () {display(8);});
+document.getElementById("number9").addEventListener("click", function () {display(9);});
+document.getElementById("number0").addEventListener("click", function () {display(0);});
+
+document.getElementById("add").addEventListener("click", function () {display('+');});
+document.getElementById("subtract").addEventListener("click", function () {display('-');});
+document.getElementById("divide").addEventListener("click", function () {display('/');});
+document.getElementById("multiply").addEventListener("click", function () {display('*');});
+document.getElementById("decimal").addEventListener("click", function () {display('.');});
